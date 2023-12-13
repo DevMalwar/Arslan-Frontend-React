bcfc22388aa869bd0377ece50e037b45891dc265
DevMalwar <150520059+DevMalwar@users.noreply.github.com>
2023-12-13 13:49:52 +0600

    Add files via upload

---
+++ React_Arslan/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Component, Component2 } from './Component';
+import Button from './Button';
+import Header from './Header';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <Header />
+        Welcome
+        <Component />
+        <Component2 />
+        <Button button={'Нажми на меня'} />
+        <Button button={'OK'} />
+        <Button button={'Отмена'} />
+      </h1>
+
+      <Footer content={'© 2023 Мой Футер'} />
+    </div>
+  );
+}
+
+export default App;
